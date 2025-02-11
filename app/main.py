63eed97cd93c97231facb3096b204c0cd88be19e
CodyMan0 <hys83751952@gmail.com>
2025-02-11 13:36:48 +0900

    [FEAT] 몽고 엔진 클래스 구현

--- app/main.py
+++ app/main.py
@@ -1,11 +1,27 @@
+from contextlib import asynccontextmanager
 from pathlib import Path
 
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from starlette.responses import HTMLResponse
 
+from app.models import mongodb
+
+DATABASE_URL = "mongodb://localhost:27017"
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("DB 연결 중")
+    await mongodb.connect()
+    yield
+    await mongodb.close()
+    print("DB 연결 헤제")
+
+
 BASE_DIR = Path(__file__).resolve().parent
-app = FastAPI()
+
+app = FastAPI(lifespan=lifespan)
 print(BASE_DIR)
 
 templates = Jinja2Templates(directory=BASE_DIR / "templates")
